2467dd35fc893712918a7ba5d628a504557ddf5e
Ian MacDowell <Ian.MacDowell@rga.com>
2013-02-28 16:46:49 -0600

    removed some files for testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http_port = process.env.PORT || 3000;
 var http = require('http');
 var start = function(){
 	var http = require('http')
-	var port = process.env.PORT || 1337;
+	var port = process.env.PORT || 3000;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
