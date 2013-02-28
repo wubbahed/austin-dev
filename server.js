776ed94b60e03526d2b284156862662c88a71315
Ian MacDowell <Ian.MacDowell@rga.com>
2013-02-28 16:44:15 -0600

    testing server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var http_port = process.env.PORT || 3000;
 var http = require('http');
 var start = function(){
+	var http = require('http')
+	var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
 	
-	
-	
+	/*
  var server = http.createServer(function (req, res) {
   require('./router').get(req, res);
  });// end server()
  server.listen(http_port);
- console.log('listening to http://localhost:' + http_port);
+ console.log('listening to http://localhost:' + http_port);*/
 };// end start()
 
 exports.start = start;
