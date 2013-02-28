fe2bb53790c9762beb03a0d6b84b468254db2cef
Ian MacDowell <Ian.MacDowell@rga.com>
2013-02-28 15:43:23 -0600

    better file handling

--- router.js
+++ router.js
@@ -3,8 +3,51 @@ var fs = require('fs');
 exports.get = function(req, res) {
  req.requrl = url.parse(req.url, true);
  var path = req.requrl.pathname;
+ 
+ var path = url.parse(req.url).pathname;
+    switch (path){
+        case '/':
+           require('./controllers/home').get(req, res);
+            break;
 
+        default:
+            if (/\.(js|html|swf)$/.test(path)){
+                try {
+                    var swf = path.substr(-4) === '.swf';
+                    res.writeHead(200, {'Content-Type': swf ? 'application/x-shockwave-flash' : ('text/' + (path.substr(-3) === '.js' ? 'javascript' : 'html'))});
+                    res.write(fs.readFileSync(__dirname + path, swf ? 'binary' : 'utf8'), swf ? 'binary' : 'utf8');
+                    res.end();
+                } catch(e){ 
+                    send404(res); 
+                }               
+                break;
+            }
+            else if (/\.(css)$/.test(path)){
+			    res.writeHead(200, {'Content-Type': 'text/css'});
+			    res.write(fs.readFileSync(__dirname + path, 'utf8')); // <--- add this line
+			    res.end();
+			    break;
+            }
+             else if (/\.(png|jpg)$/.test(path)){
+             	
+			    res.writeHead(200, {'Content-Type': 'text/'+ (path.substr(-3) === '.png' ? 'png' : 'jpg')});
+			    var img = fs.readFileSync(__dirname + path);
+			    res.end(img, 'binary');
+			    break;
+            }
+
+             require('./controllers/404').get(req, res);
+            break;
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+/*
  if ((/\.(css)$/.test(path)) || (/\.(js)$/.test(path)) || (/\.(png)$/.test(path))){
+ 	
  	if(/\.(css)$/.test(path)){
   		res.writeHead(200, {'Content-Type': 'text/css'});
   		fs.readFile(__dirname + path, 'utf8', function (err, data){
@@ -12,6 +55,7 @@ exports.get = function(req, res) {
    			res.write(data, 'utf8');
    			res.end();
    		});
+   		
    	}else if (/\.(js)$/.test(path)){
    		res.writeHead(200, {'Content-Type': 'text/javascript'});
   		fs.readFile(__dirname + path, 'utf8', function (err, data){
@@ -19,6 +63,7 @@ exports.get = function(req, res) {
    			res.write(data, 'utf8');
    			res.end();
    		});
+   		
    	}else {
    		var img = fs.readFileSync(__dirname + path);
      	res.writeHead(200, {'Content-Type': 'image/png' });
@@ -27,10 +72,13 @@ exports.get = function(req, res) {
   
   
   } else {
+  	
   if (path === '/' || path === '/home') {
    require('./controllers/home').get(req, res);
+   
   } else {
    require('./controllers/404').get(req, res);
+   
   }
- }
+ }*/
 }
