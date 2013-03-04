a8aaf3c0c6427c9652216b09eb1bd5d346afde97
Ian MacDowell <Ian.MacDowell@rga.com>
2013-03-04 16:49:44 -0600

    added node-gyp to help with time(seemed to be  dependency)

---
+++ build/time/build/Release/.deps/Release/obj.target/time/src/time.o.d
@@ -0,0 +1,29 @@
+cmd_Release/obj.target/time/src/time.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__DARWIN_UNIX03' '-DHAVE_TM_GMTOFF' '-DBUILDING_NODE_EXTENSION' -I/Users/ianma/.node-gyp/0.8.20/src -I/Users/ianma/.node-gyp/0.8.20/deps/uv/include -I/Users/ianma/.node-gyp/0.8.20/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/time/src/time.o.d.raw  -c -o Release/obj.target/time/src/time.o ../src/time.cc
+Release/obj.target/time/src/time.o: ../src/time.cc \
+  /Users/ianma/.node-gyp/0.8.20/deps/v8/include/v8.h \
+  /Users/ianma/.node-gyp/0.8.20/deps/v8/include/v8stdint.h \
+  /Users/ianma/.node-gyp/0.8.20/src/node.h \
+  /Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv.h \
+  /Users/ianma/.node-gyp/0.8.20/deps/uv/include/ares.h \
+  /Users/ianma/.node-gyp/0.8.20/deps/uv/include/ares_version.h \
+  /Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv-private/uv-unix.h \
+  /Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv-private/ngx-queue.h \
+  /Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv-private/ev.h \
+  /Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv-private/eio.h \
+  /Users/ianma/.node-gyp/0.8.20/src/node_object_wrap.h \
+  /Users/ianma/.node-gyp/0.8.20/src/ev-emul.h \
+  /Users/ianma/.node-gyp/0.8.20/src/eio-emul.h
+../src/time.cc:
+/Users/ianma/.node-gyp/0.8.20/deps/v8/include/v8.h:
+/Users/ianma/.node-gyp/0.8.20/deps/v8/include/v8stdint.h:
+/Users/ianma/.node-gyp/0.8.20/src/node.h:
+/Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv.h:
+/Users/ianma/.node-gyp/0.8.20/deps/uv/include/ares.h:
+/Users/ianma/.node-gyp/0.8.20/deps/uv/include/ares_version.h:
+/Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv-private/uv-unix.h:
+/Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv-private/ngx-queue.h:
+/Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv-private/ev.h:
+/Users/ianma/.node-gyp/0.8.20/deps/uv/include/uv-private/eio.h:
+/Users/ianma/.node-gyp/0.8.20/src/node_object_wrap.h:
+/Users/ianma/.node-gyp/0.8.20/src/ev-emul.h:
+/Users/ianma/.node-gyp/0.8.20/src/eio-emul.h:
